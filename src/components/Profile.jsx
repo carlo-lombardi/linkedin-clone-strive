bff84c2e9a2278a706294e64709621522a10aa19
slavko <slavkoj6@gmail.com>
2021-04-06 16:09:53 +0200

    layout for profile page,people col and learn col

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,18 @@
+import LearningColumn from "./LearningColumn";
+import PeopleColumn from "./PeopleColumn";
 export default function () {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="container bg-light">
+      <div className="row vh-100">
+        <div className="left col d-flex flex-column bg-warning col-12 col-lg-8 px-2">
+          {/* components on left go here */}
+        </div>
+        <div className="right col d-flex flex-column bg-light col-12 col-lg-4 px-2">
+          <PeopleColumn className="mb-2" />
+          <LearningColumn className="mb-2" />
+        </div>
+      </div>
+      <div className="row">{/* footer goes here */}</div>
     </div>
   );
 }
