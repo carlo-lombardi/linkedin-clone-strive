6fc666ea2acacbffe7966087e917810bcfe108b4
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-04-07 18:25:41 +0300

    try to fix footer

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,24 +8,26 @@ import Footer from "./Footer";
 
 export default function Profile() {
 	return (
-		<div className="container">
-			<div className="row vh-100">
-				<div className="left col d-flex flex-column col-12 col-lg-8 px-2">
-					{/* components on left go here */}
-					<ExperienceAndEdu />
-					<Skills />
-					<Acomplishments />
-					<Interests />
+		<>
+			<div className="container">
+				<div className="row vh-100">
+					<div className="left col d-flex flex-column col-12 col-lg-8 px-2">
+						{/* components on left go here */}
+						<ExperienceAndEdu />
+						<Skills />
+						<Acomplishments />
+						<Interests />
+					</div>
+					<div className="right col d-flex flex-column col-12 col-lg-4 px-2">
+						<PeopleColumn className="mb-2" />
+						<LearningColumn className="mb-2" />
+					</div>
 				</div>
-				<div className="right col d-flex flex-column col-12 col-lg-4 px-2">
-					<PeopleColumn className="mb-2" />
-					<LearningColumn className="mb-2" />
+				<div className="row d-flex flex-row">
+					{/* footer goes here */}
+					<Footer />
 				</div>
 			</div>
-			<div className="row">
-				{/* footer goes here */}
-				<Footer />
-			</div>
-		</div>
+		</>
 	);
 }
