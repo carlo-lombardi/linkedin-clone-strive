1b7ccadd865e62a5a5f55a2f0fb58a7e41800b6f
bioback <78535998+bioback@users.noreply.github.com>
2021-04-07 14:10:38 +0200

    add fetch to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,41 @@ import React from 'react'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 
 class Header extends React.Component {
+    state = {
+        isLoading: false,
+        isError: false,
+        user: []
+    }
+    
+    componentDidMount = async () => {
+        try {
+            let response = await fetch('https://striveschool-api.herokuapp.com/api/profile/606d93759f7fce00153ad1d0',
+                {headers: {
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZkOTM3NTlmN2ZjZTAwMTUzYWQxZDAiLCJpYXQiOjE2MTc3OTM5MTAsImV4cCI6MTYxOTAwMzUxMH0.drHNeuAuRKP7Hh2ong_ccbm0uDVmCwNa8CZNE5DF5Z8"
+                }}
+            )
+            console.log(response)
+            if (response.ok) {
+                let user = await response.json()
+                this.setState({
+                    user
+                })
+                console.log(user)
+            } else {
+                this.setState({
+                    isLoading: false,
+                    isError: true
+                })
+            }
+        } catch (error) {
+            this.setState({
+                isLoading: false,
+                isError: true
+            })
+            console.log(error)
+        }
+    }
+    
     render() {
         return (
             <Container>
@@ -11,7 +46,7 @@ class Header extends React.Component {
                     </Image>
                 </Row>
                 <Row className="justify-content-end mb-5">
-                    <Image className="profile" src="https://via.placeholder.com/200x200">
+                    <Image className="profile" src={this.state.user.image}>
                     </Image>
                     <Button className="headerButton mr-3 my-3" variant="primary">Connect</Button>
                     <Button className="headerButton mr-3 my-3" variant="outline-secondary">Message</Button>
@@ -19,13 +54,12 @@ class Header extends React.Component {
                 </Row>
                 <Row>
                     <Col className="flex-column">
-                        <p>Sichao. Edrea Li</p>
-                        <p>ISP Channel Manager - Huawei Technology</p>
-                        <p>Stockholm County, Sweden</p>
+                        <p>{this.state.user.name} {this.state.user.surname}</p>
+                        <p>{this.state.user.title}</p>
+                        <p>{this.state.user.area}</p>
                     </Col>
                     <Col className="flex-column">
-                        <p>Huawei</p>
-                        <p>KTH Royal Institute</p>
+                        <p>{this.state.user.bio}</p>
                     </Col>
                 </Row>
             </Container>
