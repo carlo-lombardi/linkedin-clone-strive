8d34e95c36474ffa37ebe39da5961324910d0c88
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-04-08 12:57:33 +0300

    added recomandation section

---
+++ src/components/RecomandedItem.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import { FaPlus, FaCheck } from "react-icons/fa";
+
+class RecomandedItem extends React.Component {
+	state = {
+		follow: "false",
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="row my-1 fs-6">
+					<div className="col col-2 px-2">
+						<Image roundedCircle fluid src={this.props.image} alt="" />
+					</div>
+					<div className="col col-10 p-1 flex-column d-flex ">
+						<div className="name-wrapper d-flex" style={{ lineHeight: "2px" }}>
+							<p className="font-weight-bold text-left m-0">{`${this.props.name} ${this.props.surname}`}</p>
+						</div>
+
+						<small className="text-muted text-left m-2 ">
+							{this.props.title}
+						</small>
+						<Button
+							size="sm"
+							className="rounded-pill my-1 font-weight-bold"
+							variant="outline-secondary"
+							style={{ width: "40%" }}
+							onClick={() => this.setState({ follow: !this.state.follow })}
+						>
+							{this.state.follow === "false" ? (
+								<p className="m-0">
+									<FaPlus /> Follow
+								</p>
+							) : (
+								<p className="m-0">
+									<FaCheck /> Following
+								</p>
+							)}
+						</Button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+export default RecomandedItem;
