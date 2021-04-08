9dd1f106b159231af580cfa15066b37d81241fd0
slavko <slavkoj6@gmail.com>
2021-04-08 16:30:39 +0200

    posting functionality

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,21 +1,73 @@
 import React from "react";
 import { Button, Image } from "react-bootstrap";
-import { HiOutlinePhotograph } from "react-icons/hi"
-import { ImCalendar, ImPlay } from "react-icons/im"
-import { RiArticleFill } from "react-icons/ri"
+import { HiOutlinePhotograph } from "react-icons/hi";
+import { ImCalendar, ImPlay } from "react-icons/im";
+import { RiArticleFill } from "react-icons/ri";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import { useState } from "react";
+import { submitPost } from "../functions/functions";
 
 export default (props) => {
+  const [postText, setPostText] = useState("");
+  async function handleSubmit(event) {
+    if (event.key === "Enter" && postText !== "") {
+      event.target.value = "";
+      console.log("trying to submit");
+      await submitPost(props.auth, postText);
+      await props.refreshData();
+    }
+  }
   return (
     <div className="bg-white border px-3" style={{ borderRadius: "4px" }}>
       <div className="row my-2">
-          <div className="col col-2 text-center"><Image src="http://via.placeholder.com/48x48" style={{ borderRadius: "24px" }}></Image></div>
-          <div className="col col-10"><Button className="w-100 h-100 text-left" variant="light" style={{ "border": '1px solid lightgrey' , "borderRadius": "10px" }}>Start a post</Button></div>
+        <div className="col col-2 text-center">
+          <Image src={props.profileData.image} style={{ borderRadius: "24px", height: "48px", width: "48px" }}></Image>
+        </div>
+        <div className="col col-10">
+          <InputGroup
+            className="w-100 h-100 text-left"
+            variant="light"
+            style={{ border: "1px solid lightgrey", borderRadius: "10px" }}
+          >
+            <FormControl
+              as="textarea"
+              aria-label="With textarea"
+              onChange={(event) => {
+                setPostText(event.target.value.trim());
+              }}
+              onKeyDown={(event) => {
+                handleSubmit(event);
+              }}
+            />
+          </InputGroup>
+        </div>
       </div>
       <div className="row my-2">
-          <div className="col"><Button className="text-nowrap" variant="light"><HiOutlinePhotograph className="post-icons photo-icon"/>Photo</Button></div>
-          <div className="col"><Button className="text-nowrap" variant="light"><ImPlay className="post-icons video-icon"/>Video</Button></div>
-          <div className="col"><Button className="text-nowrap" variant="light"><ImCalendar className="post-icons event-icon"/>Event</Button></div>
-          <div className="col"><Button className="text-nowrap" variant="light"><RiArticleFill className="post-icons article-icon"/>Write Article</Button></div>
+        <div className="col">
+          <Button className="text-nowrap" variant="light">
+            <HiOutlinePhotograph className="post-icons photo-icon" />
+            Photo
+          </Button>
+        </div>
+        <div className="col">
+          <Button className="text-nowrap" variant="light">
+            <ImPlay className="post-icons video-icon" />
+            Video
+          </Button>
+        </div>
+        <div className="col">
+          <Button className="text-nowrap" variant="light">
+            <ImCalendar className="post-icons event-icon" />
+            Event
+          </Button>
+        </div>
+        <div className="col">
+          <Button className="text-nowrap" variant="light">
+            <RiArticleFill className="post-icons article-icon" />
+            Write Article
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/functions/functions.js
+++ src/functions/functions.js
@@ -48,3 +48,18 @@ export async function editPost(id, auth, editText) {
     return alert("Post edited successfully");
   }
 }
+
+export async function submitPost(auth, postText) {
+  const payload = { text: postText };
+  console.log(payload);
+  const url = "https://striveschool-api.herokuapp.com/api/posts/";
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: { Authorization: auth, "Content-Type": "application/json" },
+  });
+  console.log(response);
+  if (response.ok) {
+    return alert("Post successfuly submitted");
+  }
+}
